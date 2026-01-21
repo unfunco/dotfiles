54d81d5a3f42b99ac2112a050f7a5b95e4dd7450
Daniel Morris <daniel@honestempire.com>
2026-01-21 15:19:47 +0000

    chore: Use neovim as the $EDITOR default

--- zsh/configs/editor.zsh
+++ zsh/configs/editor.zsh
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2026 Daniel Morris <daniel@honestempire.com>
 # SPDX-License-Identifier: MIT
 
-export EDITOR=vim
+export EDITOR=nvim
 export GIT_EDITOR=$EDITOR
 export VISUAL=$EDITOR
